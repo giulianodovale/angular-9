b4a69a98e782bc198ae7ce22e64b1ce6ef14fb9c
giulianodovale <giulianodovale@hotmail.com>
2020-11-04 23:29:24 -0300

    corrigindo commit e refazendo projeto

---
+++ frontend/src/app/components/product/product.service.ts
@@ -0,0 +1,34 @@
+import { Product } from './product.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  baseUrl = "  http://localhost:3000/products"
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
+
+  showMessage(msg: string): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top"
+    })
+    console.log(msg)
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product);
+  }
+
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
+  }
+
+}
